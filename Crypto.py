82a3ec967efedcec317a246b35258441f2e6453e
niks007-cryp <66521445+niks007-cryp@users.noreply.github.com>
2022-03-21 10:05:44 +0530

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -1,14 +1,12 @@
-
- import streamlit as st
- from datetime import date
+import streamlit as st
+from datetime import date
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
  
- import yfinance as yf
- from fbprophet import Prophet
- from fbprophet.plot import plot_plotly
- from plotly import graph_objs as go
- 
- START = "2015-01-01"
- TODAY = date.today().strftime("%Y-%m-%d")
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
  
  st.title('CRYPTO Forecast App')
  
